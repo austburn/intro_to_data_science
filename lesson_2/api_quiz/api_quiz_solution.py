c3b478cb25743a9cb197ac8308d32d6eb9b1a399
austburn <austburn@gmail.com>
2015-03-24 09:13:23 -0400

    Lesson 2 API Quiz.

--- lesson_2/api_quiz/api_quiz_solution.py
+++ lesson_2/api_quiz/api_quiz_solution.py
@@ -1,22 +1,20 @@
 import json
 import requests
 
-import json
-import requests
 
 def api_get_request(url):
     # In this exercise, you want to call the last.fm API to get a list of the
     # top artists in Spain.
     #
     # Once you've done this, return the name of the number 1 top artist in Spain.
-    
-    return ... # return the top artist in Spain
+
+    response = requests.get(url)
+    response = json.loads(response.text)
+    top_artist = response['topartists']['artist'][0]['name']
+
+    return top_artist
 
 
 if __name__ == '__main__':
-	# url should be the url to the last.fm api call which
-	# will return find the top artists in Spain
-
-	url = # fill this in 
-    print api_get_request(url) 
-
+    url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&format=json&api_key=6f8f282fa5f4e90f6eadbfd88f1a77af'
+    print api_get_request(url)
